8746ffbee9aebfa14ab1444d1328b68ca373f43e
Jithin C <jith97972@gmail.com>
2024-01-22 09:31:23 +0530

    added

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL=process.env.NODE_ENV==="production"?'/api':"https://homeonrentback.onrender.com"
+const baseURL=process.env.NODE_ENV==="production"?'/api':import.meta.env.VITE_BACKEND_URL
 const axiosConfig = axios.create({
   baseURL,
   withCredentials: true,   
