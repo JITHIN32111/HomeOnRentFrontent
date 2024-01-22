d644debb3666fabce7f65a0079ab159576b6748b
Jithin C <jith97972@gmail.com>
2024-01-22 12:55:23 +0530

    url changed agian

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,8 +1,18 @@
+// import axios from "axios";
+// const baseURL=process.env.NODE_ENV==="production"?'/api':import.meta.env.VITE_BACKEND_URL
+// const axiosConfig = axios.create({
+//   baseURL,
+//   withCredentials: true,   
+// });
+
+// export default axiosConfig;
 import axios from "axios";
-const baseURL=process.env.NODE_ENV==="production"?'/api':"https://homeonrentback.onrender.com"
+
+const baseURL = process.env.NODE_ENV === "production" ? '/api' :'https://homeonrentback.onrender.com/api';
+
 const axiosConfig = axios.create({
   baseURL,
-  withCredentials: true,   
+  withCredentials: true,
 });
 
 export default axiosConfig;
